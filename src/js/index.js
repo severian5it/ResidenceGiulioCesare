5865013420c96b32f3356c42663db33b029274a4
Pierluca <pierluca@amazon.com>
2019-12-20 17:22:44 +0000

    using cdn for jquery and bootstrap

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,15 @@
 // Modified from a tutorial found at
 // https://www.abeautifulsite.net/smoothly-scroll-to-an-element-without-a-jquery-plugin-2
 
+
+
+$('#contact-form').on('submit', function(e) {
+    console.log('here')
+    $('#contactFormModal').modal('show');
+    e.preventDefault();
+})
+
+
 $('a[href^="#"]').on('click', function(event) {
 
     var target = $(this.getAttribute('href'));
@@ -14,8 +23,3 @@ $('a[href^="#"]').on('click', function(event) {
     }
 
 });
-
-$('#contact-form').on('submit', function(e) {
-    $('#contactFormModal').modal('show');
-    e.preventDefault();
-})
