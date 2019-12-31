6222604c12a7051720544b0653d9733204088afc
Pierluca <pierluca@amazon.com>
2019-12-31 13:04:10 +0000

    remofactoring modal names

--- src/js/index.js
+++ src/js/index.js
@@ -25,12 +25,12 @@ $('a[href^="#"]').on('click', function (event) {
 
 // Open the Modal
 function openModal() {
-    document.getElementById("myModal").style.display = "block";
+    document.getElementById("galleryModal").style.display = "block";
 }
 
 // Close the Modal
 function closeModal() {
-    document.getElementById("myModal").style.display = "none";
+    document.getElementById("galleryModal").style.display = "none";
 }
 
 var slideIndex = 1;
@@ -48,8 +48,8 @@ function currentSlide(n) {
 
 function showSlides(n) {
     var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("demo");
+    var slides = document.getElementsByClassName("gallerySlides");
+    var dots = document.getElementsByClassName("gallery");
     var captionText = document.getElementById("caption");
     if (n > slides.length) {
         slideIndex = 1
